6bfec5df340cefb4d4e26394487777d7cd01058f
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-09 04:36:04 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,19 +35,19 @@ def can_be_created_in_a_block(args = {"title" => "Home Alone", "release_date" =>
 end
 
 def can_get_the_first_item_in_the_database
-  
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find_by(id: 1)
 end
 
 def can_find_by_multiple_attributes

3a097096331fc613366ef2a06b2e8aef26e670e3
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-09 04:43:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -69,7 +69,7 @@ def can_be_found_updated_and_saved
   movie = Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
-  Movie.save
+  movie.save
 end
 
 def can_update_using_update_method

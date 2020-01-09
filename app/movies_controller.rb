77352d2f8aa72d345c57074fcde7f366b3ff61b7
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-09 04:49:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,7 +92,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie = Movie.find_by(title: "The ")
+  movie = Movie.find_by(title: "")
   __
 end
 
